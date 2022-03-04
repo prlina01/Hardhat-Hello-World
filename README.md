a3b46e456f9f48ef89579f17d96a2537ea8bc60c
prlina01 <36077702+prlina01@users.noreply.github.com>
2022-03-04 17:44:12 +0100

    Add method to verify smart contract on etherscan

---
+++ README.md
@@ -0,0 +1,11 @@
+### Setup
+- install `npm` and `npx` on your machine
+- run `npm install` to set up all the dependencies (hardhat, ethers, etc.)
+- create`.env` file and then fill in the environment variables with your own info
+- set up an Alchemy account [here](https://alchemy.com/?a=641a319005)
+- set up a [Metamask](https://metamask.io/download.html) wallet with [fake testnet ether](https://www.rinkebyfaucet.com//)
+
+### Run:
+- run `npx hardhat run scripts/deploy.js` to deploy the contract to the Rinkeby testnet
+- run `npx hardhat run scripts/interact.js` to read and write a new message to the smart contract on Rinkeby
+- run `npx hardhat verify --network ropsten <your deplooyment address> 'Hello World!'` or call `verify` method in `interact.js` to verify your contract on Etherscan
--- scripts/interact.js
+++ scripts/interact.js
@@ -1,4 +1,4 @@
-const { ethers } = require('hardhat');
+const { ethers, run} = require('hardhat');
 
 const API_KEY= process.env.API_KEY;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
@@ -11,14 +11,21 @@ const signer = new ethers.Wallet(PRIVATE_KEY, alchemyProvider);
 
 const helloWorldContract = new ethers.Contract(CONTRACT_ADDRESS, contract.abi, signer)
 
+const verify = async () => {
+    const message = await helloWorldContract.message();
+    await run('verify:verify', {
+        address: CONTRACT_ADDRESS,
+        constructorArguments: [
+            message
+        ]
+    })
+}
 
 async function main() {
-
-
     const message = await helloWorldContract.message();
     console.log('message: ' + message)
     console.log('Updating the message....')
-    const tx = await helloWorldContract.update('Hello. This is the new message!')
+    const tx = await helloWorldContract.update('Updated message VOL 2!')
     await tx.wait();
 
     const newMessage = await helloWorldContract.message();
